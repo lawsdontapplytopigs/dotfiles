cc7dcedf1cd31e02edb3f2b180641c0f4ab72ba3
lawsdontapplytopigs <lawsdontapplytopigs@gmail.com>
2019-06-23 12:56:12 +0300

    separated the titlebars from the main rc.lua. It's also more obvious and easier to extend now

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -25,7 +25,7 @@ require("awful.autofocus")
 local wibox = require("wibox")
 -- Notification library
 local naughty = require("naughty")
-local radical = require("radical")
+local titlebars = require("titlebars")
 
 -- kill the previously running 'subscribed' scripts
 -- otherwise they'll accumulate across awesome-wm restarts
@@ -60,11 +60,9 @@ end
 do
     local in_error = false
     awesome.connect_signal("debug::error", function (err)
-
         -- Make sure we don't go into an endless error loop
         if in_error then return end
         in_error = true
-
         naughty.notify({ title = "OWIE",
                          text = tostring(err),
                          font = beautiful.font
@@ -80,6 +78,8 @@ end
 
 screen.connect_signal('refresh', function(c) return c end)
 
+
+local floating_terminal = "st -c fst"
 -------------------
 -- RULES
 -------------------
@@ -88,94 +88,116 @@ awful.rules.rules = {
     -- All clients will match this rule.
     { rule = { },
         properties = { 
-                       -- border_width = beautiful.border_width,
-                       -- border_color = beautiful.border_normal,
-                       border_width = 1,
-                       -- border_color = "#161a13",
-                       border_color = '#201e2a',
-                       focus = awful.client.focus.filter,
-                       raise = true,
-                       keys = keys.globalkeys,
-                       buttons = keys.clientbuttons,
-                       maximized_vertical = false,
-                       maximized_horisontal = false,
-                       screen = awful.screen.preferred,
-                       placement = awful.placement.no_overlap+awful.placement.no_offscreen,
-                       size_hints_honor = false
-                     }
-    },
-
-    -- Add titlebars to normal clients and dialogs
-    { rule_any = {type = { "normal", "dialog" }
-      }, properties = { titlebars_enabled = true }
-    },
-
-    -- Floating clients.
-    { rule_any = {
-        instance = {
-          "DTA",  -- Firefox addon DownThemAll.
-          "copyq",  -- Includes session name in class.
-        },
-        class = {
-          "Arandr",
-          "Gpick",
-          "Kruler",
-          "MessageWin",  -- kalarm.
-          "Sxiv",
-          "Wpa_gui",
-          "pinentry",
-          "veromix",
-          "xtightvncviewer"},
-
-        name = {
-          "Event Tester",  -- xev.
-        },
-        role = {
-          "AlarmWindow",  -- Thunderbird's calendar.
-          "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
+            -- border_width = beautiful.border_width,
+            -- border_color = beautiful.border_normal,
+            border_width = 1,
+            -- border_color = "#161a13",
+            border_color = '#201e2a',
+            focus = awful.client.focus.filter,
+            raise = true,
+            keys = keys.globalkeys,
+            buttons = keys.clientbuttons,
+            maximized_vertical = false,
+            maximized_horisontal = false,
+            screen = awful.screen.preferred,
+            placement = awful.placement.no_overlap+awful.placement.no_offscreen,
+            size_hints_honor = false
         }
-      }, properties = { floating = true }}
-
-    -- Set Firefox to always map on the tag named "2" on screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { screen = 1, tag = "2" } },
+    },
+    { -- Add titlebars to normal clients and dialogs
+        rule_any = { type = { "normal", "dialog" } }, 
+        properties = { titlebars_enabled = true }
+    },
+    { -- Floating clients.
+        rule_any = {
+            instance = {
+              "DTA",  -- Firefox addon DownThemAll.
+              "copyq",  -- Includes session name in class.
+            },
+            class = {
+                -- "Arandr",
+                "Lxappearance",
+                "Nm-connection-editor",
+                "Gpick",
+                "Kruler",
+                "MessageWin",  -- kalarm.
+                "Sxiv",
+                "Wpa_gui",
+                "pinentry",
+                "veromix",
+                "xtightvncviewer",
+                "fst"
+            },
+            name = {
+              "Event Tester",  -- xev.
+            },
+            role = {
+              "AlarmWindow",  -- Thunderbird's calendar.
+              "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
+            },
+        }, 
+        properties = { floating = true, ontop = false }
+    },
+    { 
+        rule_any = {
+            type = { "dialog" },
+            class = {
+                "Steam",
+                "discord",
+            },
+            role = {
+                "GtkFileChooserDialog",
+            }
+        }, 
+        properties = {},
+        callback = function(c)
+            awful.placement.centered(c, {honor_workarea = true})
+        end,
+    },
+    {
+        rule_any = {
+            class = {
+                "Nautilus",
+                "Thunar",
+            },
+        },
+        except_any = {
+            type = { "dialog" } 
+        },
+        properties = { 
+            floating = true, 
+            width = awful.screen.focused().geometry.width * 0.4,
+            height = awful.screen.focused().geometry.height * 0.7,
+        }
+    },
+    {
+        rule_any = {
+            class = {
+                "feh",
+                "Sxiv",
+            },
+        },
+        properties = {
+            floating = true,
+            width = awful.screen.focused().geometry.width * 0.7,
+            height = awful.screen.focused().geometry.height * 0.7,
+        },
+        callback = function(c)
+            awful.placement.centered(c, { honor_workarea = true })
+        end,
+    },
+    { -- Set Firefox to always map on the tag named "2" on screen 1.
+        rule = { class = "Firefox" },
+        properties = { 
+            -- screen = 1, 
+            tag = "6" 
+        } 
+    },
 }
-------------------------------------------------------------------------------- RULES END
-
-------------------------------------------------------------------------------- SIGNALS
--- Signal function to execute when a new client appears.
-client.connect_signal("manage", function (c)
-    -- Set the windows at the slave,
-    -- i.e. put it at the end of others instead of setting it master.
-    -- if not awesome.startup then awful.client.setslave(c) end
-
-    if awesome.startup and
-      not c.size_hints.user_position
-      and not c.size_hints.program_position then
-        -- Prevent clients from being unreachable after screen count changes.
-        awful.placement.no_offscreen(c)
-    end
-end)
 
 -- Add a titlebar if titlebars_enabled is set to true in the rules.
-client.connect_signal("request::titlebars", function(c) -------------------------------- WHEN CONFIGURING MOUSE BUTTONS, I HAVE TO LOOK AT THIS
+client.connect_signal("request::titlebars", function(c)
 
-    -- Code that shows how to add multiple titlebars.
-    -- also shows some stuff about cairo, whici I might look at later
-    -- local my_widget = wibox.widget.base.make_widget()
-    -- function my_widget:draw(_, cr, width, height)
-        -- cr:set_operator(cairo.Operator.SOURCE)
-        -- cr:set_source(gears.color.create_linear_pattern{
-            -- from = { 0, 0 },
-            -- to = { width, 0 },
-            -- stops = {
-                -- { 0, "#f000" },
-                -- { 1, "#0f0f" },
-            -- },
-        -- })
-        -- cr:paint()
-    -- end
-    -- awful.titlebar(c, { position = "bottom" }):set_widget(my_widget)
     -- buttons for the titlebar
     local buttons = gears.table.join(
         awful.button({ }, 1, function() 
@@ -191,98 +213,32 @@ client.connect_signal("request::titlebars", function(c) ------------------------
     )
 
     local top_titlebar = awful.titlebar(c, {
-            size = beautiful.titlebar_height,
-            font = beautiful.font,
-            bg_normal = beautiful.titlebar_bg_normal,
-            bg_urgent = beautiful.titlebar_bg_urgent,
-            fg_normal = beautiful.titlebar_fg_normal,
-            fg_urgent = beautiful.titlebar_fg_urgent,
-        })
-
-    top_titlebar : setup ({
-        layout = wibox.layout.fixed.vertical,
-        {
-            widget = wibox.widget.separator,
-            color  = '#ffffff10',
-            forced_height = 1,
-        },
-        {
-            widget = wibox.container.margin,
-            bottom = 1,
-            {
-                { -- Left
-                    -- awful.titlebar.widget.iconwidget(c),
-                    utils.pad_width(14),
-                    {
-                        font = beautiful.font,
-                        widget = awful.titlebar.widget.titlewidget(c),
-                    },
-                    buttons = buttons,
-                    layout  = wibox.layout.fixed.horizontal
-                },
-                { -- Middle
-                    -- { -- Title -- ( I don't like how the title looks on top, so I took it out )
-                        -- align  = "center"
-                        -- widget = awful.titlebar.widget.titlewidget(c)
-                    -- },
-                    buttons = buttons,
-                    layout  = wibox.layout.flex.horizontal
-                },
-                { -- Right
-                    -- awful.titlebar.widget.floatingbutton (c),
-                    -- awful.titlebar.widget.stickybutton   (c),
-                    -- awful.titlebar.widget.ontopbutton    (c),
-                    {
-                        widget = wibox.container.place,
-                        {
-                            widget = wibox.container.margin,
-                            -- margins = 12,
-                            top = 13,
-                            bottom = 13,
-                            left = 6,
-                            right = 6,
-                            {
-                                layout = wibox.layout.fixed.horizontal,
-                                awful.titlebar.widget.minimizebutton(c),
-                            },
-                        },
-                    },
-                    {
-                        widget = wibox.container.place,
-                        {
-                            widget = wibox.container.margin,
-                            -- margins = 12,
-                            top = 13,
-                            bottom = 13,
-                            left = 6,
-                            right = 6,
-                            {
-                                layout = wibox.layout.fixed.horizontal,
-                                awful.titlebar.widget.maximizedbutton(c),
-                            },
-                        },
-                    },
-                    {
-                        widget = wibox.container.place,
-                        {
-                            widget = wibox.container.margin,
-                            -- margins = 12,
-                            top = 13,
-                            bottom = 13,
-                            left = 6,
-                            right = 10,
-                            {
-                                layout = wibox.layout.fixed.horizontal,
-                                awful.titlebar.widget.closebutton(c),
-                            },
-                        },
-                    },
-                    layout = wibox.layout.fixed.horizontal,
-                },
-                layout = wibox.layout.align.horizontal
-            },
-        },
+        size = beautiful.titlebar_height,
+        font = beautiful.font,
+        bg_normal = beautiful.titlebar_bg_normal,
+        bg_urgent = beautiful.titlebar_bg_urgent,
+        fg_normal = beautiful.titlebar_fg_normal,
+        fg_urgent = beautiful.titlebar_fg_urgent,
     })
+
+    top_titlebar : setup ( 
+        titlebars.normal_tbar({
+            client = c,
+        }) 
+    )
+end)
+
+-- Signal function to execute when a new client appears.
+client.connect_signal("manage", function (c)
+    -- Set the windows at the slave,
+    -- i.e. put it at the end of others instead of setting it master.
+    if not awesome.startup then awful.client.setslave(c) end
+    if awesome.startup 
+        and not c.size_hints.user_position
+        and not c.size_hints.program_position then
+            -- Prevent clients from being unreachable after screen count changes.
+            awful.placement.no_offscreen(c)
+    end
 end)
 
 -- Rounded corners
@@ -323,11 +279,8 @@ client.connect_signal("manage", function (c)
     end
 end)
 
-------------------- this has to do with coloring the borders of windows when focused
--- these color client borders when focusing and unfocusing
--- client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)    
--- client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
-------------------------------------------------------------------------------- SIGNALS END
+-- the new keygrabber api is a bit weird, so I'll keep this here
+-- as an example use case
 
 -- local shapem = wibox({
 --     x = 20,
---
+++ awesome/titlebars.lua
@@ -0,0 +1,120 @@
+
+-- global core api used:
+    -- client
+
+local awful = require("awful")
+local wibox = require("wibox")
+local gears = require("gears")
+local beautiful = require("beautiful")
+local utils = require("utils")
+local titlebars = {}
+
+function titlebars.normal_tbar( args )
+
+    local c = args.client
+    local left = {
+        -- awful.titlebar.widget.iconwidget(c),
+        utils.pad_width(14),
+        {
+            font = beautiful.font,
+            widget = awful.titlebar.widget.titlewidget(c),
+        },
+        buttons = buttons,
+        layout  = wibox.layout.fixed.horizontal,
+    }
+
+    local middle = {
+        buttons = buttons,
+        layout  = wibox.layout.flex.horizontal,
+    }
+
+    local right = {
+        -- awful.titlebar.widget.floatingbutton (c),
+        -- awful.titlebar.widget.stickybutton   (c),
+        -- awful.titlebar.widget.ontopbutton    (c),
+        layout = wibox.layout.fixed.horizontal,
+        {
+            widget = wibox.container.place,
+            {
+                widget = wibox.container.margin,
+                top = 13,
+                bottom = 13,
+                left = 6,
+                right = 6,
+                {
+                    layout = wibox.layout.fixed.horizontal,
+                    awful.titlebar.widget.minimizebutton(c),
+                },
+            },
+        },
+        {
+            widget = wibox.container.place,
+            {
+                widget = wibox.container.margin,
+                top = 13,
+                bottom = 13,
+                left = 6,
+                right = 6,
+                {
+                    layout = wibox.layout.fixed.horizontal,
+                    awful.titlebar.widget.maximizedbutton(c),
+                },
+            },
+        },
+        {
+            widget = wibox.container.place,
+            {
+                widget = wibox.container.margin,
+                top = 13,
+                bottom = 13,
+                left = 6,
+                right = 10,
+                {
+                    layout = wibox.layout.fixed.horizontal,
+                    awful.titlebar.widget.closebutton(c),
+                },
+            },
+        },
+    }
+
+    local titlebar = {
+        layout = wibox.layout.fixed.vertical,
+        {
+            widget = wibox.widget.separator,
+            color  = '#ffffff10',
+            forced_height = 1,
+        },
+        {
+            widget = wibox.container.margin,
+            bottom = 1,
+            {
+                layout = wibox.layout.align.horizontal,
+                left,
+                middle,
+                right,
+            },
+        }, 
+    }
+    return titlebar
+end
+
+return titlebars
+
+-- Code that shows how to add multiple titlebars.
+-- also shows some stuff about cairo, whici I might look at later
+-- client.connect_signal("request::titlebars", function(c)
+    -- local my_widget = wibox.widget.base.make_widget()
+    -- function my_widget:draw(_, cr, width, height)
+        -- cr:set_operator(cairo.Operator.SOURCE)
+        -- cr:set_source(gears.color.create_linear_pattern{
+            -- from = { 0, 0 },
+            -- to = { width, 0 },
+            -- stops = {
+                -- { 0, "#f000" },
+                -- { 1, "#0f0f" },
+            -- },
+        -- })
+        -- cr:paint()
+    -- end
+    -- awful.titlebar(c, { position = "bottom" }):set_widget(my_widget)
+-- end)

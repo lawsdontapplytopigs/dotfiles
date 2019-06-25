6c80ffd128136ad63a506c8ae6743f7c3e07f219
lawsdontapplytopigs <lawsdontapplytopigs@gmail.com>
2019-06-25 22:21:08 +0300

    changed the taglist widget. looks more consistent now

--- awesome/piglets/hogbar/hogbar.lua
+++ awesome/piglets/hogbar/hogbar.lua
@@ -45,12 +45,10 @@ awful.layout.layouts = {
 
 ------------------- Menubar configuration
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
-------------------------------------------------------------------------------- END MENU
 
 ------------------- Keyboard map indicator and switcher
 local mykeyboardlayout = awful.widget.keyboardlayout()
 
-------------------------------------------------------------------------------- WIBAR
 ------------------- Create a textclock widget
 local mytextclock = wibox.widget.textclock( "%H:%M" )
 mytextclock.font = font
@@ -64,11 +62,10 @@ local function make_hogbar( args )
     local middle_widget = args.middle_widget or nil
     local right_widget = args.right_widget or nil
     
-    --------------- Create the wibox
+    --------------- Create the bar
     local hogbar = awful.wibar({ 
         position = "top", 
         screen = args.s, 
-        -- width = s.width
         height = hogbar_height,
         type = 'dock',
         bg = '#00000000',
@@ -78,68 +75,63 @@ local function make_hogbar( args )
     -- TODO: get the natural width of the middle widget and scale according to that.
     --------------- Add widgets to the wibox
     hogbar:setup ({ 
-        -- widget = wibox.container.margin,
-        -- top = dpi(13),
-        -- bottom = (13),
-        -- left = dpi(20),
-        -- right = dpi(20),
         layout = wibox.layout.manual,
-        -- widget = wibox.container.background,
-        -- {
-            { -- Left widget space setup
-                point = { x = 0, y = 0 },
-                forced_width = awful.screen.focused().geometry.width/2 - 60,
-                forced_height = hogbar_height,
-                widget = wibox.container.background,
-                bg = "#33ff8800",
+        { -- Left widget space setup
+            point = { x = 0, y = 0 },
+            forced_width = awful.screen.focused().geometry.width/2 - 60,
+            forced_height = hogbar_height,
+            widget = wibox.container.background,
+            -- bg = "#33ff8800",
+            {
+                layout = wibox.layout.fixed.horizontal,
                 {
-                    layout = wibox.layout.fixed.horizontal,
-                    left_widget,
-                    -- widget = wibox.widget.textbox,
-                    -- text = "",
-                },
-            },
-            { -- Middle widget space setup
-                point = { x = awful.screen.focused().geometry.width/2 - 60, y = 0 },
-                forced_width = 120,
-                forced_height = hogbar_height,
-                widget = wibox.container.background,
-                bg = "#ff008800",
-                {
-                    top = 10,
                     widget = wibox.container.margin,
+                    top = 6,
+                    bottom = 3,
+                    left = 10,
+                        left_widget,
+                }
+            },
+        },
+        { -- Middle widget space setup
+            point = { x = awful.screen.focused().geometry.width/2 - 60, y = 0 },
+            forced_width = 120,
+            forced_height = hogbar_height,
+            widget = wibox.container.background,
+            -- bg = "#ff008800",
+            {
+                top = 10,
+                widget = wibox.container.margin,
+                {
+                    widget = wibox.container.place,
                     {
-                        widget = wibox.container.place,
-                        {
-                            layout = wibox.layout.fixed.horizontal,
-                            middle_widget,
-                        },
+                        layout = wibox.layout.fixed.horizontal,
+                        middle_widget,
                     },
                 },
             },
-            { -- Right widget space setup
-                point = { x = awful.screen.focused().geometry.width/2 + 60, y = 0 },
-                forced_width = awful.screen.focused().geometry.width/2 - 60,
-                forced_height = hogbar_height,
-                widget = wibox.container.background,
-                bg = "#00880000",
+        },
+        { -- Right widget space setup
+            point = { x = awful.screen.focused().geometry.width/2 + 60, y = 0 },
+            forced_width = awful.screen.focused().geometry.width/2 - 60,
+            forced_height = hogbar_height,
+            widget = wibox.container.background,
+            -- bg = "#00880000",
+            {
+                widget = wibox.container.margin,
+                right = 10,
                 {
-                    widget = wibox.container.margin,
-                    right = 10,
+                    widget = wibox.container.place,
+                    halign = "right",
                     {
-                        widget = wibox.container.place,
-                        halign = "right",
-                        {
-                            layout = wibox.layout.fixed.horizontal,
-                            right_widget,
-                        }
-                    },
+                        layout = wibox.layout.fixed.horizontal,
+                        right_widget,
+                    }
                 },
             },
+        },
     })
-
     return hogbar
-
 end
 
 -- Wallpaper
@@ -153,14 +145,8 @@ local function set_wallpaper(s)
         gears.wallpaper.maximized(wallpaper, s, false)
     end
 end
-
 ------------------- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
 screen.connect_signal("property::geometry", set_wallpaper)
-
-    local screen_width = awful.screen.focused().geometry.width
-    -- local timebox_width = 3 -- ???? If only you could get its natural width
-    local wibar_height = 42
-    local font_height = beautiful.get_font_height(beautiful.font)
 
 ------------------- Create a wibox for each screen and add it
 awful.screen.connect_for_each_screen(function(s)
@@ -168,202 +154,36 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Taglist
-
     -- Each screen has its own tag table.
     local names = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }
     local l = awful.layout.suit  -- Just to save some typing: use an alias.
     local layouts = { l.floating, l.floating, l.floating, l.floating, 
                       l.floating, l.floating, l.tile, l.tile, l.tile,}
-
     awful.tag(names, s, layouts)
 
     -- We need one layoutbox per screen.
     s.mylayoutbox = awful.widget.layoutbox(s)
     s.mylayoutbox:buttons(gears.table.join(
-                           awful.button({ }, 1, function () awful.layout.inc( 1) end),
-                           awful.button({ }, 3, function () awful.layout.inc(-1) end),
-                           awful.button({ }, 4, function () awful.layout.inc( 1) end),
-                           awful.button({ }, 5, function () awful.layout.inc(-1) end)))
+        awful.button({ }, 1, function () awful.layout.inc( 1) end),
+        awful.button({ }, 3, function () awful.layout.inc(-1) end),
+        awful.button({ }, 4, function () awful.layout.inc( 1) end),
+        awful.button({ }, 5, function () awful.layout.inc(-1) end)
+    ))
 
     local layoutbox = wibox.widget({
         s.mylayoutbox,
         layout = wibox.layout.fixed.horizontal,
-        
     })
 
     make_hogbar({
         screen = s, 
         height = 43, 
-        left_widget = make_left_widget(s),
+        left_widget = make_left_widget({
+            screen = s,
+            taglist_width = 300,
+        }),
         middle_widget = mytextclock,
         right_widget = make_right_widget(s),
     })
-        
 end)
 
-
--- CODE TO REVIEW LATER
--- local tasklist_buttons = gears.table.join(
-                     -- awful.button({ }, 1, function (c)
-                                              -- if c == client.focus then
-                                                  -- c.minimized = true
-                                              -- else
-                                                  -- Without this, the following
-                                                  -- :isvisible() makes no sense
-                                                  -- c.minimized = false
-                                                  -- if not c:isvisible() and c.first_tag then
-                                                      -- c.first_tag:view_only()
-                                                  -- end
-                                                  -- -- This will also un-minimize
-                                                  -- -- the client, if needed
-                                                  -- client.focus = c
-                                                  -- c:raise()
-                                              -- end
-                                          -- end),
-                     -- awful.button({ }, 3, client_menu_toggle_fn()),
-                     -- awful.button({ }, 4, function ()
-                                              -- awful.client.focus.byidx(1)
-                                          -- end),
-                     -- awful.button({ }, 5, function ()
-                                              -- awful.client.focus.byidx(-1)
-                                          -- end))
-
-    -- Create a taglist widget
-    -- local taglist = awful.widget.taglist({
-        -- screen = s, 
-        -- filter = awful.widget.taglist.filter.all, 
-        -- buttons = taglist_buttons,
-        -- layout = wibox.layout.fixed.horizontal,
-        -- widget_template = {
-            -- {
-                -- {
-                    -- id = 'index_role',
-                    -- widget = wibox.widget.textbox,
-                -- },
-                -- margins = 3,
-                -- widget = wibox.container.margin,
-            -- },
-            -- widget = wibox.container.background,
-            -- bg = '#201e2a',
-            -- shape = gears.shape.circle,
-            -- create_callback = function(self, c3, index, objects)
-                -- self:get_children_by_id('index_role')[1].markup = '<b> ' .. index .. ' </b>'
-                -- self:connect_signal('mouse::enter', function()
-                    -- self.bg = '#aa0000'
-                -- end)
-                -- self:connect_signal('mouse::leave', function()
-                    -- self.bg = '#201e2a'
-                -- end)
-                -- 
-            -- end,
-        -- }
-    -- })
-
--- helper functions (even though it's just one for now)
--- local function client_menu_toggle_fn()
-    -- local instance = nil
--- 
-    -- return function ()
-        -- if instance and instance.wibox.visible then
-            -- instance:hide()
-            -- instance = nil
-        -- else
-            -- instance = awful.menu.clients({ theme = { width = 250 } })
-        -- end
-    -- end
--- end
-
--- Menu (The right-click one)
--- Create a launcher widget and a main menu
--- local myawesomemenu = {
-   -- { "hotkeys", function() return false, hotkeys_popup.show_help end},
-   -- { "manual", terminal .. " -e man awesome" },
-   -- { "edit config", editor_cmd .. " " .. awesome.conffile },
-   -- { "restart", awesome.restart },
-   -- { "quit", function() awesome.quit() end}
--- }
--- 
--- local menu_awesome = { "awesome", myawesomemenu, beautiful.awesome_icon }
--- local menu_terminal = { "open terminal", terminal }
-
--- if has_fdo then
-    -- mymainmenu = freedesktop.menu.build({
-        -- before = { menu_awesome },
-        -- after =  { menu_terminal }
-    -- })
-
--- local mymainmenu = awful.menu({
-    -- items = {
-              -- menu_awesome,
-              -- menu_terminal,
-            -- }
--- })
-
--- Menu (The right-click one)
--- Create a launcher widget and a main menu
--- local myawesomemenu = {
-   -- { "hotkeys", function() return false, hotkeys_popup.show_help end},
-   -- { "manual", terminal .. " -e man awesome" },
-   -- { "edit config", editor_cmd .. " " .. awesome.conffile },
-   -- { "restart", awesome.restart },
-   -- { "quit", function() awesome.quit() end}
--- }
--- 
--- local menu_awesome = { "awesome", myawesomemenu, beautiful.awesome_icon }
--- local menu_terminal = { "open terminal", terminal }
-
--- if has_fdo then
-    -- mymainmenu = freedesktop.menu.build({
-        -- before = { menu_awesome },
-        -- after =  { menu_terminal }
-    -- })
-
--- local mymainmenu = awful.menu({
-    -- items = {
-              -- menu_awesome,
-              -- menu_terminal,
-            -- }
--- })
-
--- Menu (The right-click one)
--- Create a launcher widget and a main menu
--- local myawesomemenu = {
-   -- { "hotkeys", function() return false, hotkeys_popup.show_help end},
-   -- { "manual", terminal .. " -e man awesome" },
-   -- { "edit config", editor_cmd .. " " .. awesome.conffile },
-   -- { "restart", awesome.restart },
-   -- { "quit", function() awesome.quit() end}
--- }
--- 
--- local menu_awesome = { "awesome", myawesomemenu, beautiful.awesome_icon }
--- local menu_terminal = { "open terminal", terminal }
-
--- if has_fdo then
-    -- mymainmenu = freedesktop.menu.build({
-        -- before = { menu_awesome },
-        -- after =  { menu_terminal }
-    -- })
-
--- local mymainmenu = awful.menu({
-    -- items = {
-              -- menu_awesome,
-              -- menu_terminal,
-            -- }
--- })
-
--- local mymenu = radical.context({ })
--- mymenu:add_item ({text = "ses1", button1 = function(_menu,item,mods) print("Hello world") end})
--- mymenu:add_item ({text = "Submenu", sub_menu = function()
-    -- local smenu = radical.context({})
-    -- smenu:add_item({text = "submenu string1"})
-    -- smenu:add_item({text = "not submenu haha"})
-    -- return smenu
--- end})
--- mytextclock:set_menu(mymenu, "button::pressed", 3)
-
-------------------- Launcher
--- NOTE AF: IDK WHY THIS IS CALLED A LAUNCHER, WHEN IT'S ACTUALLY THE IMAGE
--- THAT YOU CAN CLICK ON THAT SHOWS YOU THE MENU (THE ONE THAT SHOWS WHEN YOU RIGHT CLICK)
--- `AWFUL.WIDGET.PROMPT` IS ACTUALLY THE LAUNCHER (SIMILAR TO DMENU)
--- local mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
-                                     -- menu = mymainmenu })
--- awesome/piglets/hogbar/left_widget.lua
+++ awesome/piglets/hogbar/left_widget.lua
@@ -1,6 +1,42 @@
 local awful = require("awful")
 local wibox = require("wibox")
 local gears = require("gears")
+local utils = require("utils")
+local naughty = require("naughty")
+
+-- beautiful.taglist_fg_focus
+-- beautiful.taglist_bg_focus
+-- beautiful.taglist_fg_urgent
+-- beautiful.taglist_bg_urgent
+-- beautiful.taglist_bg_occupied
+-- beautiful.taglist_fg_occupied
+-- beautiful.taglist_bg_empty
+-- beautiful.taglist_fg_empty
+-- beautiful.taglist_bg_volatile
+-- beautiful.taglist_fg_volatile
+-- beautiful.taglist_squares_sel
+-- beautiful.taglist_squares_unsel
+-- beautiful.taglist_squares_sel_empty
+-- beautiful.taglist_squares_unsel_empty
+-- beautiful.taglist_squares_resize
+-- beautiful.taglist_disable_icon
+-- beautiful.taglist_font
+-- beautiful.taglist_spacing
+-- beautiful.taglist_shape
+-- beautiful.taglist_shape_border_width
+-- beautiful.taglist_shape_border_color
+-- beautiful.taglist_shape_empty
+-- beautiful.taglist_shape_border_width_empty
+-- beautiful.taglist_shape_border_color_empty
+-- beautiful.taglist_shape_focus
+-- beautiful.taglist_shape_border_width_focus
+-- beautiful.taglist_shape_border_color_focus
+-- beautiful.taglist_shape_urgent
+-- beautiful.taglist_shape_border_width_urgent
+-- beautiful.taglist_shape_border_color_urgent
+-- beautiful.taglist_shape_volatile
+-- beautiful.taglist_shape_border_width_volatile
+-- beautiful.taglist_shape_border_color_volatile
 
 local taglist_buttons = gears.table.join(
     awful.button({ }, 1, function(t) t:view_only() end),
@@ -21,63 +57,63 @@ local taglist_buttons = gears.table.join(
     awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end)
 )
 
-local make_left_widget = function(screen)
-    local s = screen
+local make_left_widget = function( args )
 
-    -- local mytaglist = awful.widget.taglist({
-        -- screen  = s,
-        -- filter  = awful.widget.taglist.filter.all,
-        -- buttons = taglist_buttons,
-        -- layout = { layout = wibox.layout.fixed.horizontal,},
-        -- widget_template = {
-            -- layout = wibox.layout.fixed.horizontal,
-            -- id = "background_role",
-            -- widget = wibox.container.background,
-            -- bg = '#8800ff',
-            -- {
-                -- layout = wibox.layout.fixed.horizontal,
-                -- id = "index_role",
-                -- widget = wibox.widget.textbox,
-            -- },
-        -- },
-    -- })
-
+    local taglist_width = args.taglist_width
+    local s = args.screen
     local mytaglist = awful.widget.taglist {
         screen  = s,
         filter  = awful.widget.taglist.filter.all,
         style   = { 
-            shape = gears.shape.rectangle, 
-            bg_focus = '#ffd143',
-            -- fg_empty = '#b48800',
-            fg_occupied = '#b48800',
-            fg_focus = '#111111',
+            -- shape = gears.shape.rounded_bar, 
+            bg_focus = '#1a1823',
+            fg_empty = '#ffffff',
+            fg_occupied = '#120020',
+            fg_focus = '#ffffff',
+            shape_border_width = 0,
+            -- shape_border_color = 
+            -- shape_empty
+            -- shape_border_width_empty = 
+            -- shape_border_color_empty
+            -- shape_focus
+            shape_border_width_focus = 1,
+            shape_border_color_focus = "#0e0818",
+            shape_urgent
+            -- shape_border_width_urgent = 
+            -- shape_border_color_urgent
+            -- shape_volatile
+            -- shape_border_width_volatile
+            -- shape_border_color_volatile
         },
-        layout   = { layout  = wibox.layout.fixed.horizontal, },
+        layout = { layout = wibox.layout.flex.horizontal, },
         widget_template = {
-            id     = 'background_role',
+            id = "my_bg",
             widget = wibox.container.background,
+            shape  = utils.rrect(6),
             forced_width = 40,
             {
-                layout = wibox.layout.flex.horizontal,
+                id = 'background_role',
+                widget = wibox.container.background,
+                forced_width = taglist_width / 9,
+                bg = '#1a1823',
                 {
                     widget = wibox.container.place,
                     {
-                        id     = 'text_role',
-                        font = "Roboto Bold 13",
+                        id = "text_role",
+                        font = "Roboto Bold 12",
                         widget = wibox.widget.textbox,
-                    },
-                }
+                    }
+                },
             },
-
             -- Add support for hover colors and an index label
             create_callback = function(self, c3, index, objects) --luacheck: no unused args
-                self:get_children_by_id('text_role')[1].markup = '<b> '..index..' </b>'
+                self:get_children_by_id("text_role")[1].markup = '<b> '..index..' </b>'
                 self:connect_signal('mouse::enter', function()
                     if self.bg ~= '#00000000' then
                         self.backup_bg     = '#00000000'
                         self.backup_fg     = '#ffffffff'
                     end
-                    self.bg = '#ffd143'
+                    self.bg = '#2a104c'
                     self.fg = '#111111'
                 end)
                 self:connect_signal('mouse::leave', function()
@@ -88,13 +124,12 @@ local make_left_widget = function(screen)
                 end)
             end,
             update_callback = function(self, c3, index, objects) --luacheck: no unused args
-                self:get_children_by_id('text_role')[1].markup = '<b> '..index..' </b>'
+                self:get_children_by_id("text_role")[1].markup = '<b> '..index..' </b>'
             end,
         },
         buttons = taglist_buttons
     }
     return mytaglist
-
 end
 
 return make_left_widget
